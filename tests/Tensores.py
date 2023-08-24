c932370a3ca41196a5855e289cf586d7bc52bd96
epereirasg <112261482+epereirasg@users.noreply.github.com>
2023-08-24 18:09:07 -0400

    Update Tensores.py

--- tests/Tensores.py
+++ tests/Tensores.py
@@ -1,17 +1,1 @@
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-import tensorflow as tf
-
-import numpy as np
-
-real=[1,2,3],[4,5,6],[7,8,9]
-strings=["loco","loco","loco"],["loco","loco","loco"],["loco","loco","loco"]
-permutary=[19,20,21],[22,23,24],[25,26,27]
-binary=[0,1,1],[1,0,1],[1,1,1]
-
-
-y=np.array([[real],[strings],[binary]])
-
-print(y)
 
